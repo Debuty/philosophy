490762e8aa0297988b1071d0c5e01a9c079a8442
Mostafa Abdelrahman <mostafa.abdelrahman@intellisec-me.com>
2025-09-02 18:57:38 +0300

    Fix To Debly Part 2

--- src/modules/auth/signup/signup.tsx
+++ src/modules/auth/signup/signup.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
-import { useNavigate } from 'react-router-dom';
+
 import { useTranslation } from 'react-i18next';
 // @ts-ignore
 import { supabase } from '../../../supabaseClient';
